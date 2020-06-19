1bb380bca88bdd36523c0b9897d139c249f2d792
Oanhs <phamvanoanh26@gmail.com>
2020-06-18 17:59:27 -0700

    add all file in qt IQ project

---
+++ main/testqt.h
@@ -0,0 +1,26 @@
+#ifndef TESTQT_H
+#define TESTQT_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class testQT; }
+QT_END_NAMESPACE
+
+class testQT : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    testQT(QWidget *parent = nullptr);
+    ~testQT();
+
+private:
+    Ui::testQT *ui;
+
+
+private slots:
+    void onOpenFile();
+
+};
+#endif // TESTQT_H
